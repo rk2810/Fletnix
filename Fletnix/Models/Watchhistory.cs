4fae4046267daf040a808a95b481f722d45c577e
Sander Koenders <sanderkoenders@gmail.com>
2017-05-12 10:30:27 +0200

    Scaffold

---
+++ Fletnix/Models/Watchhistory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fletnix.Models
+{
+    public partial class Watchhistory
+    {
+        public int MovieId { get; set; }
+        public string CustomerMailAddress { get; set; }
+        public DateTime WatchDate { get; set; }
+        public decimal Price { get; set; }
+        public bool Invoiced { get; set; }
+
+        public virtual Customer CustomerMailAddressNavigation { get; set; }
+        public virtual Movie Movie { get; set; }
+    }
+}
